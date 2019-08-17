03dcbc04904eb72ad9b8ec68169996589222d82d
Stanislaw Karkavin <me@xdefrag.dev>
2019-08-17 16:53:38 +0300

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Entities;
+
+namespace InfinityDialogue
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private World _world;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _world = new WorldBuilder()
+                .Build();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _world.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _world.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
